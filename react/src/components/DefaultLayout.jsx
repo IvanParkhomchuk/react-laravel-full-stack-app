a876486892926cd09758accbedd1f974e7a8e3a2
Ivan <workaccbyivan@gmail.com>
2023-07-14 14:48:41 +0300

    'feature/context_API'

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -3,6 +3,8 @@ import {Outlet} from "react-router-dom";
 export default function DefaultLayout() {
     return (
         <>
+            <p>default</p>
+
             <Outlet />
         </>
     )
--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -3,6 +3,8 @@ import {Outlet} from "react-router-dom";
 export default function GuestLayout() {
     return (
         <>
+            <p>guest</p>
+
             <Outlet />
         </>
     )
