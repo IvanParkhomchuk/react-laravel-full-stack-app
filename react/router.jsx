c87cc80af2e2d484e24c44307b7eaf80548ffcb3
Ivan <workaccbyivan@gmail.com>
2023-07-14 14:13:45 +0300

    'feature/multiple_layouts'

--- react/router.jsx
+++ react/router.jsx
@@ -1,25 +1,48 @@
-import { createBrowserRouter } from 'react-router-dom';
+import {createBrowserRouter, Navigate} from 'react-router-dom';
 import Login from "./src/views/Login";
 import Signup from "./src/views/Signup";
 import Users from "./src/views/Users";
 import NotFound from "./src/views/NotFound";
+import DefaultLayout from "./src/components/DefaultLayout";
+import GuestLayout from "./src/components/GuestLayout";
+import Dashboard from "./src/views/Dashboard";
 
 const router = createBrowserRouter([
     {
-        path: '/login',
-        element: < Login />
+        path: '/',
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Navigate to='/users' />
+            },
+            {
+                path: '/users',
+                element: <Users />
+            },
+            {
+                path: '/dashboard',
+                element: <Dashboard />
+            }
+        ],
     },
     {
-        path: '/signup',
-        element: < Signup />
-    },
-    {
-        path: '/users',
-        element: < Users />
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: '/signup',
+                element: <Signup />
+            },
+            {
+                path: '/login',
+                element: <Login />
+            },
+        ],
     },
     {
         path: '*',
-        element: < NotFound />
+        element: <NotFound />
     },
 ]);
 
