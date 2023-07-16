a5812293a3cd6ce0ebd4cfe0e54873878d79950c
Ivan <workaccbyivan@gmail.com>
2023-07-16 17:26:18 +0300

    'feature/implement_login'

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,8 +1,44 @@
 import {Link} from "react-router-dom";
+import {useRef, useState} from "react";
+import axiosClient from "../axios-client.js";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
 
 export default function Login() {
+    const emailRef = useRef()
+    const passwordRef = useRef()
+
+    const {setUser, setToken} = useStateContext()
+
+    const [errors, setErrors] = useState(null)
+
     const onSubmit = (e) => {
         e.preventDefault();
+
+        const payload = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        }
+
+        setErrors(null);
+
+        axiosClient.post('/login', payload)
+            .then(({data}) => {
+                setUser(data.user)
+                setToken(data.token);
+            })
+            .catch(err => {
+                const response = err.response;
+
+                if (response && response.status === 422) {
+                    if (response.data.errors) {
+                        setErrors(response.data.errors);
+                    } else {
+                        setErrors({
+                            email: [response.data.message]
+                        });
+                    }
+                }
+            });
     };
 
     return (
@@ -13,8 +49,16 @@ export default function Login() {
                         Login into your account
                     </h1>
 
-                    <input type="email" placeholder={'Email'}/>
-                    <input type="password" placeholder={'Password'}/>
+                    {errors &&
+                        <div className="alert">
+                            {Object.keys(errors).map(key => (
+                                <p key={key}>{errors[key][0]}</p>
+                            ))}
+                        </div>
+                    }
+
+                    <input ref={emailRef} type="email" placeholder={'Email'}/>
+                    <input ref={passwordRef} type="password" placeholder={'Password'}/>
 
                     <button className={'btn btn-block'}>Login</button>
 
