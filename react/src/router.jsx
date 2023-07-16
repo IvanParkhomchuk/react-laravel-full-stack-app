16edf9dbb2ed124bae8ec935513730274f5ab46a
Ivan <workaccbyivan@gmail.com>
2023-07-16 16:38:17 +0300

    'feature/implement_signup'

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,11 +1,11 @@
 import {createBrowserRouter, Navigate} from 'react-router-dom';
-import Login from "./src/views/Login";
-import Signup from "./src/views/Signup";
-import Users from "./src/views/Users";
-import NotFound from "./src/views/NotFound";
-import DefaultLayout from "./src/components/DefaultLayout";
-import GuestLayout from "./src/components/GuestLayout";
-import Dashboard from "./src/views/Dashboard";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
+import Users from "./views/Users";
+import NotFound from "./views/NotFound";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
+import Dashboard from "./views/Dashboard";
 
 const router = createBrowserRouter([
     {
--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,8 +1,40 @@
 import {Link} from "react-router-dom";
+import {useRef, useState} from "react";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
+import axiosClient from "../axios-client.js";
 
 export default function Signup() {
+    const nameRef = useRef()
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmationRef = useRef()
+
+    const {setUser, setToken} = useStateContext()
+
+    const [errors, setErrors] = useState(null)
+
     const onSubmit = (e) => {
         e.preventDefault();
+
+        const payload = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
+        }
+
+        axiosClient.post('/signup', payload)
+            .then(({data}) => {
+                setUser(data.user)
+                setToken(data.token);
+            })
+            .catch(err => {
+                const response = err.response;
+
+                if (response && response.status === 422) {
+                    setErrors(response.data.errors)
+                }
+            });
     };
 
     return (
@@ -13,10 +45,18 @@ export default function Signup() {
                         Signup for free
                     </h1>
 
-                    <input type="text" placeholder={'Full name'}/>
-                    <input type="email" placeholder={'Email'}/>
-                    <input type="password" placeholder={'Password'}/>
-                    <input type="password" placeholder={'Password Confirmation'}/>
+                    {errors &&
+                        <div className="alert">
+                            {Object.keys(errors).map(key => (
+                                <p key={key}>{errors[key][0]}</p>
+                            ))}
+                        </div>
+                    }
+
+                    <input ref={nameRef} type="text" placeholder={'Full name'}/>
+                    <input ref={emailRef} type="email" placeholder={'Email'}/>
+                    <input ref={passwordRef} type="password" placeholder={'Password'}/>
+                    <input ref={passwordConfirmationRef} type="password" placeholder={'Password Confirmation'}/>
 
                     <button className={'btn btn-block'}>Signup</button>
 
